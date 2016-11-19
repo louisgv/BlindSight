91eaff488285336669f10dc8925ac67244016b27
Louis Vichy <lgvichy@gmail.com>
2016-11-19 18:26:54 -0500

    We will take a look at this later. Adding a slide view tooling

---
+++ BlindSight/app/src/main/java/com/lab/blindsight/ScreenSlidePageFragment.java
@@ -0,0 +1,24 @@
+package com.lab.blindsight;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by lab on 11/13/16.
+ */
+
+public class ScreenSlidePageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(
+                R.layout.activity_stream, container, false);
+
+        return rootView;
+    }
+}
+
